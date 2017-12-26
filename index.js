ce9f8f55274cb1dcc00824e2116a97aa4fce1352
Sarwar Chahal <axiom24@users.noreply.github.com>
2017-12-26 14:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4177,66 +4177,6 @@ function onmyway () {
   init(data)
 }
 
-function no () {
-  "use strict";
-
-  // type 'no' on your keyboard
-  var key = [78,79];
-  var ck = 0;
-  var max = key.length;
-
-  var no = function() {
-
-    var shock = document.createElement('div');
-    var img = new Image;
-    img.src = data;
-    img.style.width = '500px';
-    img.style.height = '500px';
-    img.style.transition = '1s all';
-    img.style.position = 'fixed';
-    img.style.left = 'calc(50% - 250px)';
-    img.style.bottom = '-600px';
-    img.style.zIndex = 999999;
-
-    document.body.appendChild(img);
-
-    window.setTimeout(function(){
-      img.style.bottom = '0px';
-    },30);
-
-    window.setTimeout(function(){
-      img.style.bottom = '-600px';
-    }, 4300);
-    window.setTimeout(function(){
-      img.parentNode.removeChild(img);
-      shock.parentNode.removeChild(shock);
-    }, 5400);
-
-  };
-
-  var record = function(e) {
-
-    if ( e.which === key[ck] ) {
-      ck++;
-    } else {
-      ck = 0;
-    }
-
-    if ( ck >= max ) {
-      no();
-      ck = 0;
-    }
-
-  };
-
-  var init = function(data) {
-    document.addEventListener('keyup', record);
-  };
-
-  var data = 'https://weichiachang.github.io/easter-eggs-mobile/images/no.gif'
-
-  init(data);
-}
 
 function kirby () {
   "use strict"
@@ -5290,66 +5230,6 @@ function wink () {
     init(data);
 }
 
-function hi () {
-  "use strict";
-  
-    // type 'hi' on your keyboard
-    var key = [72,73];
-    var ck = 0;
-    var max = key.length;
-  
-    var hi = function() {
-  
-      var shock = document.createElement('div');
-      var img = new Image;
-      img.src = data;
-      img.style.width = '300px';
-      img.style.height = '300px';
-      img.style.transition = '1s all';
-      img.style.position = 'fixed';
-      img.style.left = 'calc(50% - 150px)';
-      img.style.bottom = '-100px';
-      img.style.zIndex = 999999;
-  
-      document.body.appendChild(img);
-  
-      window.setTimeout(function(){
-        img.style.bottom = '-50px';
-      },30);
-  
-      window.setTimeout(function(){
-        img.style.bottom = '-300px';
-      }, 4300);
-      window.setTimeout(function(){
-        img.parentNode.removeChild(img);
-      }, 5400);
-  
-    };
-  
-    var record = function(e) {
-  
-      if ( e.which === key[ck] ) {
-        ck++;
-      } else {
-        ck = 0;
-      }
-  
-      if ( ck >= max ) {
-        hi();
-        ck = 0;
-      }
-  
-    };
-  
-    var init = function(data) {
-      document.addEventListener('keyup', record);
-    };
-  
-    var data = 'https://weichiachang.github.io/easter-eggs-mobile/images/hi.gif';
-  
-    init(data);
-}
-
 bravo();
 britney();
 doggy();
@@ -5392,7 +5272,6 @@ snorlax();
 board();
 haters();
 onmyway();
-no();
 kirby();
 hangover();
 balloon();
@@ -5410,6 +5289,5 @@ haha();
 kumamon();
 unicorn();
 wink();
-hi();
 cityflo();
 zipgo();
